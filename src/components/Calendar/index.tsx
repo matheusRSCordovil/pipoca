71352d142f5d155a737b041858fa2615c4a6b891
trotou <tico.ribeiro0396@gmail.com>
2022-08-16 17:12:14 -0300

    first commit

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { DayDiv } from "./styles";
+
+const monthsDays = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+const Calendar = () => {
+  const [currentMonth, setCurrentMonth] = React.useState<number>(
+    new Date().getMonth() + 1
+  );
+
+  const handleCurrentMonth = (month: string) => {
+    setCurrentMonth(Number(month));
+  };
+
+  return (
+    <div>
+      <select onChange={(e) => handleCurrentMonth(e.target.value)}>
+        <option value="1">January</option>
+        <option value="2">February</option>
+        <option value="3">March</option>
+        <option value="4">April</option>
+        <option value="5">May</option>
+        <option value="6">June</option>
+        <option value="7">July</option>
+        <option value="8">August</option>
+        <option value="9">September</option>
+        <option value="10">October</option>
+        <option value="11">November</option>
+        <option value="12">December</option>
+      </select>
+
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {Array.from(Array(monthsDays[currentMonth]), (_, i) => i + 1).map(
+          (day) => (
+            <DayDiv>
+              <p>{day}</p>
+            </DayDiv>
+          )
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Calendar;
---
+++ src/components/Calendar/styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const DayDiv = styled.div`
+  display: flex;
+  height: 43.1489372253418px;
+  width: 43.1489372253418px;
+  border-radius: 1.6595743894577026px;
+  background-color: #f1f1f1;
+  color: #7d7d7d;
+  margin: 4px;
+  font-size: 0.5rem;
+  font-weight: bold;
+  justify-content: right;
+  align-items: end;
+
+  p {
+    margin: 3px;
+    height: 8px;
+  }
+`;
