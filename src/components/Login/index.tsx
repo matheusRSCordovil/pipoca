9acecd9bd13f9c21bdb66414c17d2ad58f12c4fb
trotou <tico.ribeiro0396@gmail.com>
2022-09-05 12:21:04 -0300

    FEAT: tela de registro

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -3,8 +3,15 @@ import { Carousel } from "react-responsive-carousel";
 import { MainContainer } from "./styles";
 import NextIcon from "../../assets/icon/play.fill.svg";
 import PipocaPuppet from "../../assets/img/pipoca-puppet.png";
+import PipocaPuppetV22 from "../../assets/img/pipoca-puppet-v22.png";
+import PipocaPuppetV4 from "../../assets/img/pipoca-puppet-v4.png";
+import PipocaPuppetV3 from "../../assets/img/pipoca-puppet-v3.png";
+import CellImg2 from "../../assets/img/group407.png";
+import CellImg1 from "../../assets/img/group409.png";
+import CellImg5 from "../../assets/img/group414.png";
+import CalendarImg from "../../assets/img/group404.png";
 import MutedtIcon from "../../assets/img/speaker.png";
-import Ellipse from "../../assets/img/ellipse-19.png";
+import AadaLogo from "../../assets/icon/logo-aada.png";
 import { CSSProperties } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -81,170 +88,278 @@ const Login = () => {
         }
       >
         <div
-          style={{
-            backgroundColor: "#1391ED",
-            height: "100vh",
-            display: "flex",
-            flexDirection: "column",
-            backgroundImage: `url(${Ellipse})`,
-            backgroundPositionX: "center",
-            backgroundPositionY: "bottom",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "contain",
-          }}
+          className="login-container"
+          style={{ minHeight: "100vh", width: "100%", height: "100%" }}
         >
           <img
-            alt="muted-icon"
-            src={MutedtIcon}
+            style={{ width: "106px" }}
+            alt="aada-icon"
+            src={AadaLogo}
+            className="aada-icon"
+          />
+          <img
             style={{
               width: "38px",
-              height: "39px",
-              position: "absolute",
-              right: "20px",
-              top: "20px",
             }}
+            alt="muted-icon"
+            className="muted-icon"
+            src={MutedtIcon}
           />
-          <p>Olá!</p>
+          <div className="text-1-container">
+            <h4>Olá!</h4>
 
-          <p>Bem-vindo à Jornada da Pipoca Atópica! </p>
+            <p style={{ marginBottom: "0" }}>
+              Bem-vindo à Jornada da Pipoca Atópica!
+            </p>
 
-          <p>Como podemos te chamar?</p>
-          <input
-            type="text"
+            <p>Como podemos te chamar?</p>
+            <input
+              type="text"
+              style={{
+                width: "354px",
+                height: "50px",
+                margin: "0 auto",
+                border: "none",
+                borderRadius: "20.5px",
+                boxShadow: "0px 4px 4px 0px #00000040",
+                zIndex: 100,
+              }}
+            />
+          </div>
+
+          <div className="bottom-div-1">
+            <img
+              alt="pipoca-puppet"
+              src={PipocaPuppet}
+              style={{
+                width: "292px",
+                height: "248px",
+                position: "absolute",
+                bottom: "80px",
+                left: "15%",
+              }}
+            />
+          </div>
+        </div>
+
+        <div className="login-container-2">
+          <img
+            style={{ width: "106px" }}
+            alt="aada-icon"
+            src={AadaLogo}
+            className="aada-icon"
+          />
+          <img
             style={{
-              width: "354px",
-              height: "50px",
-              margin: "0 auto",
-              border: "none",
-              borderRadius: "20.5px",
-              boxShadow: "0px 4px 4px 0px #00000040",
+              width: "38px",
             }}
+            alt="muted-icon"
+            className="muted-icon"
+            src={MutedtIcon}
           />
+
+          <div className="text-2-container">
+            <h4>Oi Ana,</h4>
+            <p>Verificamos aqui que esta é sua primeira jornada!</p>
+            <p>
+              Nosso objetivo é conhecer melhor sua vida com a dermatite atópica
+              e dar alguns conselhos de como melhorá-la.
+            </p>
+            <p>A Pipoca Atópica será nossa grande aliada nesta jornada!</p>
+          </div>
           <img
             alt="pipoca-puppet"
-            src={PipocaPuppet}
-            style={{ width: "292px", height: "248px", margin: "0 auto" }}
-          />
-        </div>
-        <div
-          style={{
-            backgroundColor: "blue",
-            height: "100vh",
-            display: "flex",
-            alignItems: "center",
-          }}
-        >
-          <img
-            alt="muted-icon"
-            src={MutedtIcon}
+            src={PipocaPuppetV22}
             style={{
-              width: "38px",
-              height: "39px",
+              width: "260px",
+              height: "260px",
               position: "absolute",
-              right: "20px",
-              top: "20px",
+              bottom: "80px",
+              right: "0",
             }}
           />
-          <img
-            alt="dasdasd"
-            src="https://media.moddb.com/images/members/5/4550/4549205/duck.jpg"
-          />
         </div>
-        <div
-          style={{
-            backgroundColor: "green",
-            height: "100vh",
-            display: "flex",
-            alignItems: "center",
-          }}
-        >
+
+        <div className="login-container-3">
           <img
-            alt="muted-icon"
-            src={MutedtIcon}
+            style={{ width: "106px" }}
+            alt="aada-icon"
+            src={AadaLogo}
+            className="aada-icon"
+          />
+          <img
             style={{
               width: "38px",
-              height: "39px",
-              position: "absolute",
-              right: "20px",
-              top: "20px",
             }}
-          />
-          <img
-            alt="asdd"
-            src="https://media.moddb.com/images/members/5/4550/4549205/duck.jpg"
-          />
-        </div>
-        <div
-          style={{
-            backgroundColor: "green",
-            height: "100vh",
-            display: "flex",
-            alignItems: "center",
-          }}
-        >
-          <img
             alt="muted-icon"
+            className="muted-icon"
             src={MutedtIcon}
+          />
+
+          <img
+            style={{
+              position: "absolute",
+              width: "350px",
+              transform: "translate(6%,16%)",
+            }}
+            src={CalendarImg}
+            alt="calendario-imagem"
+          />
+
+          <p style={{ position: "absolute" }}>
+            <span>Durante 30 dias</span>, vamos te ajudar a acompanhar sua vida
+            com a dermatite atópica.
+          </p>
+
+          <div className="bottom-div-3">
+            <img
+              alt="pipoca-puppet"
+              src={PipocaPuppetV4}
+              style={{
+                width: "218px",
+                height: "204px",
+                position: "absolute",
+                bottom: "220px",
+                right: "3px",
+              }}
+            />
+            <p>
+              Ao final dessa jornada vamos poder avaliar juntos como melhorar os
+              seus cuidados e sua qualidade de vida.
+            </p>
+          </div>
+        </div>
+
+        <div className="login-container-4">
+          <img
+            style={{ width: "106px" }}
+            alt="aada-icon"
+            src={AadaLogo}
+            className="aada-icon"
+          />
+          <img
             style={{
               width: "38px",
-              height: "39px",
-              position: "absolute",
-              right: "20px",
-              top: "20px",
             }}
-          />
-          <img
-            alt="asdd"
-            src="https://media.moddb.com/images/members/5/4550/4549205/duck.jpg"
-          />
-        </div>
-        <div
-          style={{
-            backgroundColor: "green",
-            height: "100vh",
-            display: "flex",
-            alignItems: "center",
-          }}
-        >
-          <img
             alt="muted-icon"
+            className="muted-icon"
             src={MutedtIcon}
+          />
+          <p className="text-4-top">
+            E para fazer esse acompanhamento <span>É FÁCIL!</span>
+          </p>
+          <div className="top-div-4">
+            <span>
+              <p>
+                É só ir na área de registros e contar como você esta se
+                sentindo.
+              </p>
+            </span>
+            <img
+              alt="pipoca-puppet"
+              src={CellImg1}
+              style={{
+                width: "200px",
+                height: "319px",
+                position: "absolute",
+                bottom: "-34px",
+                left: "3px",
+              }}
+            />
+          </div>
+          <div className="bottom-div-4">
+            <p>
+              Aqui você pode nos contar de tudo, desde como está sua pele, seu
+              humor, e até quais medicamentos esta usando!
+            </p>
+            <img
+              alt="pipoca-puppet"
+              src={CellImg2}
+              style={{
+                width: "200px",
+                height: "319px",
+                position: "absolute",
+                bottom: "84px",
+                right: "3px",
+              }}
+            />
+          </div>
+        </div>
+
+        <div className="login-container-5">
+          <img
+            style={{ width: "106px" }}
+            alt="aada-icon"
+            src={AadaLogo}
+            className="aada-icon"
+          />
+          <img
             style={{
               width: "38px",
-              height: "39px",
+            }}
+            alt="muted-icon"
+            className="muted-icon"
+            src={MutedtIcon}
+          />
+          <p className="text-5-top">
+            E com esses dados, nós te mostramos a sua{" "}
+            <span>jornada e evolução!</span>
+          </p>
+          <img
+            alt="celular-imagem-5"
+            src={CellImg5}
+            style={{
+              width: "192px",
+              height: "330px",
               position: "absolute",
-              right: "20px",
-              top: "20px",
+              bottom: "235px",
+              right: "0",
             }}
           />
-          <img
-            alt="asdd"
-            src="https://media.moddb.com/images/members/5/4550/4549205/duck.jpg"
-          />
+          <div className="bottom-div-5">
+            <p>
+              Além de aprender mais sobre você e sua jornada com a dermatite
+              atópica, você ainda pode compartilhar os relatórios com seu médico
+              para ele analisar seu tratamento.
+            </p>
+          </div>
         </div>
-        <div
-          style={{
-            backgroundColor: "green",
-            height: "100vh",
-            display: "flex",
-            alignItems: "center",
-          }}
-        >
+
+        <div className="login-container-6">
           <img
-            alt="muted-icon"
-            src={MutedtIcon}
+            style={{ width: "106px" }}
+            alt="aada-icon"
+            src={AadaLogo}
+            className="aada-icon"
+          />
+          <img
             style={{
               width: "38px",
-              height: "39px",
-              position: "absolute",
-              right: "20px",
-              top: "20px",
             }}
+            alt="muted-icon"
+            className="muted-icon"
+            src={MutedtIcon}
           />
-          <img
-            alt="asdd"
-            src="https://media.moddb.com/images/members/5/4550/4549205/duck.jpg"
-          />
+          <div className="text-6-container">
+            <p>
+              Vamos começar então?
+              <br /> A Pipoca Atópica está animada para te ajudar nesta jornada!{" "}
+            </p>
+          </div>
+
+          <div className="bottom-div-6">
+            <img
+              alt="pipoca-puppet"
+              src={PipocaPuppetV3}
+              style={{
+                width: "296px",
+                height: "281px",
+                position: "absolute",
+                bottom: "241px",
+                left: "15%",
+              }}
+            />
+          </div>
         </div>
       </Carousel>
     </MainContainer>
